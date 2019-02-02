624f024ccf2040e404d6384eb1c47a1564aa7814
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-02 22:11:19 +0000

    Added scraper file.

--- lib/NBA_news.rb
+++ lib/NBA_news.rb
@@ -3,5 +3,5 @@ require "open-uri"
 require "pry"
 
 require_relative "NBA_news/version"
-#require_relative "NBA_news/articles_scraper.rb"
+require_relative "NBA_news/news_scraper.rb"
 require_relative "NBA_news/cli.rb"

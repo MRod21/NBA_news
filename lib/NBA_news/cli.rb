01083a33fb470c2f0ed3a6637f7af608046ce091
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-04 00:21:53 +0000

    Fixed scraper

--- lib/NBA_news/cli.rb
+++ lib/NBA_news/cli.rb
@@ -3,15 +3,15 @@ class CLI
   def run
     puts "Welcome to NBA News! Here are the latest stories around the league:"
     Scraper.scrape
-    list_stories
+    #list_stories
     puts "Select an article number to read the full story or exit to close."
     input = gets.strip
   end
 
-  def list_stories
-    Story.all.each_with_index do |story, i|
-      puts "#{i+1} #{story.title} By: #{story.author}"
-    end
-  end
+  #def list_stories
+  #  Story.all.each_with_index do |story, i|
+  #    puts "#{i+1} #{story.title} By: #{story.author}"
+  #  end
+  #end
 
 end
--- lib/NBA_news/scraper.rb
+++ lib/NBA_news/scraper.rb
@@ -2,14 +2,15 @@ class Scraper
 
   def self.scrape
     doc = Nokogiri::HTML(open("http://www.sportingnews.com/us/nba/news"))
-    array_of_stories = doc.css("ul.media-list")
+    array_of_stories = doc.css("div.media-body")
 
-    array_of_stories.each do |post|
+    array_of_stories.each do |info|
       story = Story.new
-      story.title = post.css("a")[2].text
-      story.author = post.css("a")[3].text
-      story.url = post.css("a")[2]["href"]
+      story.title = info.css("h3.media-heading a").text
+      story.author = info.css("span.author a").text
+      story.url = info.css("a")[2]["href"]
     end
+    binding.pry
   end
 
   def scrape_details

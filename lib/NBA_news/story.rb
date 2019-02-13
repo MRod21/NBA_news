d771e2621b9ea482ffb5ffdf3c1ea9f2501f98d0
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-13 00:40:29 +0000

    Added list authors method.

--- lib/NBA_news/story.rb
+++ lib/NBA_news/story.rb
@@ -11,6 +11,14 @@ class Story
     @@all
   end
 
+  def self.list_authors
+    self.all.map do |story|
+      story.author
+    end
+    binding.pry
+  end
+
+  #def title=()
   #def self.content
   #  system ("open '#{url}'")
   #  binding.pry

229576fe0c798ae2e7fc3217dcfda70e93607071
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-19 08:10:41 +0000

    Refactored CLI.

--- lib/NBA_news/cli.rb
+++ lib/NBA_news/cli.rb
@@ -23,7 +23,7 @@ class CLI
 
     if response == "y" || response == "yes"
       puts "What story would you like to read? (Select a number or 'exit'.)".colorize(:yellow)
-      input = gets.strip
+      input = gets.strip.downcase
       if input.to_i.between?(1, Story.all.size)
         story_object = Story.all[input.to_i-1]
         Scraper.scrape_content(story_object.url)
@@ -33,8 +33,9 @@ class CLI
         sleep 1
         list_stories
         puts "Please choose a story number that is listed or 'exit' to close.".colorize(:yellow)
+        rerun_menu
       else input == "exit"
-        puts "Goodbye!".colorize(:yellow)
+        puts "Thanks for visiting, goodbye!".colorize(:yellow)
       end
     elsif response == "n" || response == "no"
       puts "Okay, maybe next time. Thanks for stopping by!".colorize(:yellow)
@@ -44,22 +45,34 @@ class CLI
     end
   end
   
-
-  def second_menu
-    puts "Would you like to read another story?".colorize(:yellow)
-    answer = gets.strip.downcase
-    if answer ==  "y" || answer =="yes"
-      #menu
-      list_stories
-      puts "Great! Please make another selection.".colorize(:yellow)
-      input = gets.strip
+  
+  def rerun_menu
+    input = gets.strip.downcase
       if input.to_i.between?(1, Story.all.size)
         story_object = Story.all[input.to_i-1]
         Scraper.scrape_content(story_object.url)
         second_menu
-      end
+      elsif input.to_i.between?(1, Story.all.size) == false && input != "exit"
+        puts "I'm sorry that story is not listed.".colorize(:yellow)
+        sleep 1
+        list_stories
+        puts "Please choose a story number that is listed or 'exit' to close.".colorize(:yellow)
+        rerun_menu
+      else input == "exit"
+        puts "Thanks for visiting, goodbye!".colorize(:yellow)
+    end
+  end
+  
+
+  def second_menu
+    puts "Would you like to read another story? [y/n]".colorize(:yellow)
+    answer = gets.strip.downcase
+    if answer ==  "y" || answer =="yes"
+      list_stories
+      puts "Great! Please make another selection. (Enter story number)".colorize(:yellow)
+      rerun_menu
     elsif answer == "n" || answer == "no"
-      puts "Goodbye!"
+      puts "Thanks for reading, goodbye!".colorize(:yellow)
     else
       puts "I'm sorry. I did not get that.".colorize(:yellow)
       second_menu
